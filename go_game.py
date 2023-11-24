b384aebf8e89210b6584685c5ff9970dd9fd428e
Pedro "Totty" Sousa <83694082+TottyAmadeus@users.noreply.github.com>
2023-11-24 19:53:40 +0000

    repetições removidas

--- go_game.py
+++ go_game.py
@@ -1,25 +1,26 @@
 import numpy as np
 from copy import deepcopy
 
+
 class Piece:
     def __init__(self, action, player, state, args) -> None:
         self.args = args
         self.state = state
-        self.action = action # (x, y)
-        self.player = player # 1 or -1
+        self.action = action  # (x, y)
+        self.player = player  # 1 or -1
         self.x_dim = args[0]
         self.y_dim = args[1]
-        self.neighbors = self.get_neighbors(action) # list of neighbour coordinates
+        self.neighbors = self.get_neighbors(
+            action)  # list of neighbour coordinates
         self.group = self.search_group(action, player, state)
-
 
     def get_neighbors(self, action):
         x = action[0]
         y = action[1]
         neighbors = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-        neighbors = [neighbor for neighbor in neighbors if neighbor[0] >= 0 and neighbor[0] < self.x_dim and neighbor[1] >= 0 and neighbor[1] < self.y_dim]
+        neighbors = [neighbor for neighbor in neighbors if neighbor[0] >=
+                     0 and neighbor[0] < self.x_dim and neighbor[1] >= 0 and neighbor[1] < self.y_dim]
         return neighbors
-    
 
     def search_group(self, action, player, state):
 
@@ -27,36 +28,35 @@ class Piece:
         for neighbor in self.neighbors:
             if state[neighbor[0]][neighbor[1]] != 0 and state[neighbor[0]][neighbor[1]].player == player:
                 neighbors.append(state[neighbor[0]][neighbor[1]])
-        
+
         # if neighbors has no pieces of same player
         if neighbors == []:
-            group =Group(action, player, state, args)
+            group = Group(action, player, state, args)
             group.add_piece(self)
             return group
 
         groups = []
 
         for neighbor in neighbors:
-            if neighbor != 0 and neighbor.player == player: # if neighbor is same player and not empty
+            if neighbor != 0 and neighbor.player == player:  # if neighbor is same player and not empty
                 groups.append(neighbor.group)
 
         if groups == []:
             group = Group(action, player, state, args)
             group.add_piece(self)
             return group
-        
+
         else:
             if len(groups) > 1:
                 for group in groups[1:]:
                     groups[0].merge_group(group)
-        
-        
+
         groups[0].add_piece(self)
         return groups[0]
-    
+
     def __str__(self) -> str:
         return str(self.player)
-        
+
 
 class Group:
     def __init__(self, action, player, state, args) -> None:
@@ -90,7 +90,7 @@ class Group:
                     liberties.append(neighbor)
         # print("LIBERTIES: " + str(liberties))
         return set(liberties)
-    
+
     def merge_group(self, group):
         self.pieces += group.pieces
 
@@ -98,7 +98,7 @@ class Group:
             piece.group = self
 
         return self
-    
+
     def capture(self, state):
         for piece in self.pieces:
             state[piece.action[0]][piece.action[1]] = 0
@@ -117,7 +117,6 @@ class Go:
         self.previous_equals = False
         self.statelist = []
 
-
     def get_initial_state(self):
         board = []
         for i in range(self.x_dim):
@@ -125,44 +124,45 @@ class Go:
             for j in range(self.y_dim):
                 board[i].append(0)
         return board
-    
+
     def put_piece(self, state, action, piece: Piece):
-        state[action[0]][action[1]] = piece # temporary for checking
-        piece.group.liberties = piece.group.search_liberties(piece.player, state) # update liberties4,4
+        state[action[0]][action[1]] = piece  # temporary for checking
+        piece.group.liberties = piece.group.search_liberties(
+            piece.player, state)  # update liberties4,4
         # if it doesn't capture antything, remove piece and print suicide
         for neighbor in piece.neighbors:
             if state[neighbor[0]][neighbor[1]] != 0:
-                state[neighbor[0]][neighbor[1]].group.liberties = state[neighbor[0]][neighbor[1]].group.search_liberties(state[neighbor[0]][neighbor[1]], state)
+                state[neighbor[0]][neighbor[1]].group.liberties = state[neighbor[0]
+                                                                        ][neighbor[1]].group.search_liberties(state[neighbor[0]][neighbor[1]], state)
                 if state[neighbor[0]][neighbor[1]].player != piece.player:
                     if len(state[neighbor[0]][neighbor[1]].group.liberties) == 0:
                         state[neighbor[0]][neighbor[1]].group.capture(state)
 
-
         return state
-    
+
     def suicide(self, state, piece):
 
         # deepcopy the board to verify suicide
         copystate = deepcopy(state)
         copypiece = deepcopy(piece)
 
-        copystate[action[0]][action[1]] = copypiece # temporary
-        copypiece.group.liberties = copypiece.group.search_liberties(copypiece.player, copystate)
+        copystate[action[0]][action[1]] = copypiece  # temporary
+        copypiece.group.liberties = copypiece.group.search_liberties(
+            copypiece.player, copystate)
         # if it has more than 0 liberties, no suicide
-        if(len(copypiece.group.liberties) > 0):
+        if (len(copypiece.group.liberties) > 0):
             return False
-        
+
         # if it removes a enemy group, liberties above 0 so legal
         for neighbor in copypiece.neighbors:
             if copystate[neighbor[0]][neighbor[1]] != 0:
-                copystate[neighbor[0]][neighbor[1]].group.liberties = copystate[neighbor[0]][neighbor[1]].group.search_liberties(copystate[neighbor[0]][neighbor[1]].player, copystate)
+                copystate[neighbor[0]][neighbor[1]].group.liberties = copystate[neighbor[0]][neighbor[1]
+                                                                                             ].group.search_liberties(copystate[neighbor[0]][neighbor[1]].player, copystate)
                 if len(copystate[neighbor[0]][neighbor[1]].group.liberties) == 0 and copystate[neighbor[0]][neighbor[1]].player != copypiece.player:
                     # capture group if it has no liberties and is not same player
                     return False
-                
+
         return True
-        
-        
 
     def get_next_state(self, state, action, player):
         next_state = state.copy()
@@ -181,22 +181,39 @@ class Go:
         if self.suicide(state, piece):
             print("Suicide is an illegal move")
             return state
-        
+
         # print("NEIGHBOURS: " + str(piece.neighbors))
         # print("PLAYER: " + str(piece.player))
         next_state = self.put_piece(next_state, action, piece)
+        statemat = self.convert_state_to_matrix(next_state)
+        print("ADDING TO REPEAT CHECK: ")
+        self.add_matrix_to_positions(statemat)
         go.player = go.change_player()
         self.previous_equals = False
-        self.statelist.append(next_state)
 
         # print("GROUP PIECES: " + str(piece.group.pieces))
         # print("GROUP LIBERTIES: " + str(piece.group.liberties))
 
         return next_state
-    
-    def is_valid_move(self, state, action):
+
+    def is_valid_move(self, state, action, player):
         x, y = action
+
+        statecopy = deepcopy(state)
+        temppiece = Piece(action, player, statecopy, args)
+
         if state[x][y] != 0:
+            return False
+
+        self.put_piece(statecopy, action, temppiece)
+
+        # print("NEW TEMPORARY STATE:")
+        statecopy = self.convert_state_to_matrix(statecopy)
+        # print(str(statecopy))
+        # print("VERIFYING REPEATED LIST: ")
+        # print(str(self.statelist))
+        if any(np.array_equal(statecopy, stateelement) for stateelement in self.statelist):
+            print("Invalid Move: Repeated State")
             return False
         return True
 
@@ -219,45 +236,66 @@ class Go:
 
     def change_player(self):
         return -self.player
-    
+
     def get_valid_actions(self, state, player):
         valid_actions = []
         for i in range(len(state)):
             for j in range(len(state[1])):
-                if state[i,j] == 0:
-                    valid_actions.append((i,j))
+                if state[i, j] == 0:
+                    valid_actions.append((i, j))
         return valid_actions
-    
+
     def check_skip(self, state, action, player):
-        if (action == (-1,-1)):
+        if (action == (-1, -1)):
             print("Player " + str(player) + " skips")
             return True
-        
+
     def get_winner(self, state):
         pass
 
+    def add_matrix_to_positions(self, matrix):
+        print(str(matrix))
+        self.statelist.append(matrix)
+
+    def convert_state_to_matrix(self, state):
+        mat = np.zeros((self.x_dim, self.y_dim))
+        for i in range(len(state)):
+            for j in range(len(state[1])):
+                if str(state[i][j]) == "1":
+                    mat[i][j] = 1
+                elif str(state[i][j]) == "-1":
+                    mat[i][j] = -1
+        return mat
 
 
-args = [11,11]
+args = [5, 5]
 go = Go(args)
 state = go.get_initial_state()
 
 while True:
     go.print_board(state)
 
-    action = input("Input move (x,y): \n")
+    action = input("Input move x,y | -1,-1 to pass: \n")
     action = action.split(",")
+
     try:
         action = (int(action[0]), int(action[1]))
+        while (action[0] >= go.x_dim or action[1] >= go.y_dim or action[0] < -1 or action[1] < -1):
+            print("Invalid Move: Out of Bounds")
+            action = input("Input move x,y | -1,-1 to pass: \n")
+            action = action.split(",")
+            action = (int(action[0]), int(action[1]))
+
     except:
-        print("Invalid move")
+        print("Invalid Move: Not a number")
         continue
-    while (action >= (go.x_dim, go.y_dim) or action < (-1,-1) or state[action[0]][action[1]] != 0):
-        print("Invalid move")
-        action = input("Input move (x,y): \n")
+
+    while (state[action[0]][action[1]] != 0 or go.is_valid_move(state, action, go.player) == False):
+        action = input("Input move x,y | -1,-1 to pass: \n")
         action = action.split(",")
         action = (int(action[0]), int(action[1]))
 
     state = go.get_next_state(state, action, go.player)
+
     if state == -1:
         break
