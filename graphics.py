f2da79582a324a2d7a1cddc1933d1dbd4c29e6af
Barbara Santos <blsbls03@gmail.com>
2023-12-14 10:41:19 +0000

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,100 +1,98 @@
 import pygame
 
-data={'size': 9,
-      'player1':(201,153,255),
+data={'player1':(201,153,255),
       'player2':(179,236,255),
-      'white':(255,255,255),
-      'black':(0,0,0),
-      'green_go':(140, 217, 166)
       }
+
+SIZE_BOARD = 9
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (140, 217, 166)
+
 
 pygame.init()
 pygame_icon = pygame.image.load('image.png')
 pygame.display.set_icon(pygame_icon)
 
-SIDE=800
+SCREEN_SIZE=600
+SCREEN_PADDING = 50
+CELL_SIZE = (SCREEN_SIZE - SCREEN_PADDING) // SIZE_BOARD
+PIECE_SIZE = (SCREEN_SIZE - 2*SCREEN_PADDING) // SIZE_BOARD // 3
+WINDOW_TITLE = "Ms PacMan"
 
-screen=pygame.display.set_mode((SIDE,SIDE))
+screen=pygame.display.set_mode((SCREEN_SIZE,SCREEN_SIZE))
 
-pygame.display.set_caption("depression")
+pygame.display.set_caption("gO depression")
 
-def draw_board(num_size):
-    for i in range(data['size']):
-        pygame.draw.line(screen, data['black'],(SIDE*i//data['size'],0),(SIDE*i//data['size'],SIDE),3)
-        pygame.draw.line(screen, data['black'],(0,SIDE*i//data['size']),(SIDE,SIDE*i//data['size']),3)
+def to_pixels(x):
+    return SCREEN_PADDING + x*CELL_SIZE
+
+def to_coord(x):
+    quarter = CELL_SIZE//4
+    closest = (x-SCREEN_PADDING)//CELL_SIZE
+    if abs(to_pixels(closest)-(x-SCREEN_PADDING > to_pixels(closest)-(x-SCREEN_PADDING+quarter))):
+        closest = (x-SCREEN_PADDING+quarter)//CELL_SIZE
+    return closest
+
+def draw_board():
+    pygame.draw.rect(screen, GREEN, rect=(SCREEN_PADDING, SCREEN_PADDING, CELL_SIZE*(SIZE_BOARD-1), CELL_SIZE*(SIZE_BOARD-1)))
+    for i in range(SIZE_BOARD):
+        pygame.draw.line(screen, BLACK,(to_pixels(i),SCREEN_PADDING),(to_pixels(i),CELL_SIZE*(SIZE_BOARD-1) + SCREEN_PADDING),3)
+        pygame.draw.line(screen, BLACK,(SCREEN_PADDING,to_pixels(i)),(CELL_SIZE*(SIZE_BOARD-1)+SCREEN_PADDING,to_pixels(i)),3)
+
 def draw_piece(x,y,player):
-    color=data['white']
-    if player==-1:
-        color=data['black']
-    pygame.draw.circle(screen,color,(x*SIDE//data['size'],y*SIDE//data['size']),30)
-    pygame.draw.circle(screen,data['black'],(x*SIDE//data['size'],y*SIDE//data['size']),30,3)
+    color = BLACK if player == -1 else WHITE
+    pygame.draw.circle(screen,color,(to_pixels(x),to_pixels(y)),PIECE_SIZE)
+    pygame.draw.circle(screen,BLACK,(to_pixels(x),to_pixels(y)),PIECE_SIZE,3)
 
-def hover_piece(x, y, player):
+def hover_to_select(player,valid_moves,click):
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    coords = (x*SIDE//data['size'],y*SIDE//data['size'])
-    distance = pygame.math.Vector2(coords[0] - mouse_x, coords[1] - mouse_y).length()
-    if distance < 30:
-        s = pygame.Surface((SIDE, SIDE), pygame.SRCALPHA)
-        if player == 1:
-            pygame.draw.circle(s,(0,0,0,30),(x*SIDE//data['size'],y*SIDE//data['size']),30)
-        if player == -1:
-            pygame.draw.circle(s,(255,255,255,60),(x*SIDE//data['size'],y*SIDE//data['size']),30)
-        screen.blit(s, (0, 0))
-
-    pygame.draw.circle(screen,data['black'],(x*SIDE//data['size'],y*SIDE//data['size']),30,3)
-
-def hover_to_select(x,y,player):
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    coords = (x*SIDE//data['size'],y*SIDE//data['size'])
-    distance = pygame.math.Vector2(coords[0] - mouse_x, coords[1] - mouse_y).length()
-    if distance < 30:
-        s = pygame.Surface((SIDE, SIDE), pygame.SRCALPHA)
-        if player == 1:
-            pygame.draw.circle(s,(0,0,0,255),(x*SIDE//data['size'],y*SIDE//data['size']),30)
-        if player == -1:
-            pygame.draw.circle(s,(255,255,255,255),(x*SIDE//data['size'],y*SIDE//data['size']),30)
-        pygame.draw.circle(s,data['black'],(x*SIDE//data['size'],y*SIDE//data['size']),30,3)
-        screen.blit(s, (0, 0))
-
+    x, y = None, None
+    if ([to_coord(mouse_x), to_coord(mouse_y)] in valid_moves):
+        x, y = to_coord(mouse_x), to_coord(mouse_y)
     
-
-def select_piece(valid_moves, player):
-    for move in valid_moves:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        coords = (move[0]*SIDE//data['size'],move[1]*SIDE//data['size'])
-        distance = pygame.math.Vector2(coords[0] - mouse_x, coords[1] - mouse_y).length()
-        if distance < 30:
+    if (x!=None):
+        pixels = (to_pixels(x),to_pixels(y))
+        distance = pygame.math.Vector2(pixels[0] - mouse_x, pixels[1] - mouse_y).length()
+        if distance < PIECE_SIZE:
+            s = pygame.Surface((SCREEN_SIZE, SCREEN_SIZE), pygame.SRCALPHA)
             if player == 1:
-                pygame.draw.circle(screen,(0,0,0,30),(coords[0],coords[1]),30)
+                pygame.draw.circle(s,(255,255,255,255),(to_pixels(x),to_pixels(y)),PIECE_SIZE)
             if player == -1:
-                pygame.draw.circle(screen,(255,255,255),(coords[0],coords[1]),30)
-            return
-    print('invalid')
-    ...
+                pygame.draw.circle(s,(0,0,0,255),(to_pixels(x),to_pixels(y)),PIECE_SIZE)
+            pygame.draw.circle(s,BLACK,(to_pixels(x),to_pixels(y)),PIECE_SIZE,3)
+            screen.blit(s, (0, 0))
+        if click:
+            cur_pieces.append([x, y, player])
+            valid_moves.remove([x, y])
+            return [x, y, -1*player]
+    return [None, None, player]
+
+click = False
+valid_moves = []
+for i in range(SIZE_BOARD):
+    for j in range(SIZE_BOARD):
+        valid_moves.append([i, j])
+
+cur_pieces = []
+player = 1
 
 while True:
-    valid_moves = [[2, 1],[7, 4], [3,3], [6,4]]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            click = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            click = False
 
+    screen.fill(GREEN)
+    draw_board()
 
-    screen.fill(data['green_go'])
+    for piece in cur_pieces:
+        draw_piece(piece[0], piece[1], piece[2])
 
-    for i in range(data['size']):
-        pygame.draw.line(screen, data['black'],(SIDE*i//data['size'],0),(SIDE*i//data['size'],SIDE),3)
-        pygame.draw.line(screen, data['black'],(0,SIDE*i//data['size']),(SIDE,SIDE*i//data['size']),3)
-    draw_piece(1,1,-1)
-    draw_piece(2,4,1)
-    hover_piece(1,1,-1)
-    hover_piece(2,4,1)
-
-    for move in valid_moves:
-        hover_to_select(move[0], move[1], 1)
-
-    left, middle, right = pygame.mouse.get_pressed()
-    if left:
-        select_piece(valid_moves, 1)
+    x, y, player = hover_to_select(player, valid_moves, click)
 
     pygame.display.flip()
 
