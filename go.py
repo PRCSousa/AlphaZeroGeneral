104f170bf6e2f3406bcba0337a25edd94f0998d7
Pedro "Totty" Sousa <83694082+TottyAmadeus@users.noreply.github.com>
2023-12-20 02:09:06 +0000

    go adaptado a nossa architecture (provavelmente)

--- go.py
+++ go.py
@@ -9,27 +9,54 @@ class Go:
     MARKER = 4
     LIBERTY = 8
 
-    liberties = []
-    block = []
-    seki_count = 0
-    seki_liberties = []
-    run = True
-    board = None
-
-    white_captures = 0
-    black_captures = 0
-
-    def __init__(self, args) -> None:
+    def __init__(self, args: list) -> None:
+        self.args = args
         self.board_size = int(args[0])
         self.komi = float(args[1])
         self.board = self.get_initial_state()
         self.color = self.BLACK
-        self.move_count = 0
-        self.game_over = False
-        self.winner = None
         self.history = []
+        self.action_size = self.board_size * self.board_size
+
+        self.previous_skipped = False
+
+        self.black_captures = 0
+        self.white_captures = 0
+
+        self.liberties = []
+        self.block = []
+        self.seki_count = 0
+        self.seki_liberties = []
+
+
+    def check_skip_end(self, action: tuple):
+        '''
+        # Description:
+        Checks if the previous move was a skip and if the current move is also a skip.
+        If both are skips, returns True, otherwise returns False and sets the previous_skipped variable to True.
+
+        # Returns:
+        True if both moves are skips.
+
+        False if either of the moves is not a skip.
+        '''
+        if action == (-1, -1):
+            if self.previous_skipped:
+                print("True on check_skip_end")
+                return True
+            else:
+                self.previous_skipped = True
+                print("False on check_skip_end")
+                return False
 
     def get_initial_state(self):
+        '''
+        # Description:
+        Returns the initial state of the board.
+
+        # Returns:
+        A list of lists representing the board.
+        '''
         board = []
         for i in range(self.board_size):
             board.append([])
@@ -38,115 +65,142 @@ class Go:
         return board
 
 
-    def count(self,x,y,colour):
+    def count(self, x, y, state: list, player:int , liberties: list, block: list) -> tuple[list, list]:
+        '''
+        # Description:
+        Counts the number of liberties of a stone and the number of stones in a block.
+        Follows a recursive approach to count the liberties of a stone and the number of stones in a block.
+
+        # Returns:
+        A tuple containing the number of liberties and the number of stones in a block.
+        '''
         
         #initialize piece
-        piece = self.board[y][x]
+        piece = state[y][x]
         #if there's a stone at square
-        if piece and piece & colour and (piece & self.MARKER) == 0:
+        if piece and piece & player and (piece & self.MARKER) == 0:
             #save stone coords
-            self.block.append((y,x))
+            block.append((y,x))
             #mark the stone
-            self.board[y][x] |= self.MARKER
+            state[y][x] |= self.MARKER
             
             #look for neighbours recursively
-            if y-1 >= 0: self.count(x,y-1,colour) #walk north
-            if x+1 < len(self.board): self.count(x+1,y,colour) #walk east
-            if y+1 < len(self.board): self.count(x,y+1,colour) #walk south
-            if x-1 >= 0: self.count(x-1,y,colour) #walk west
+            if y-1 >= 0:
+                liberties, block = self.count(x,y-1,state,player,liberties, block) #walk north
+            if x+1 < len(self.board):
+                liberties, block = self.count(x+1,y,state,player,liberties, block) #walk east
+            if y+1 < len(self.board):
+                liberties, block = self.count(x,y+1,state,player,liberties, block) #walk south
+            if x-1 >= 0:
+                liberties, block = self.count(x-1,y,state,player,liberties, block) #walk west
 
         #if square is empty
         elif piece == self.EMPTY:
             #mark liberty
-            self.board[y][x] = self.LIBERTY
+            state[y][x] = self.LIBERTY
             #save liberties
-            self.liberties.append((y,x))
+            liberties.append((y,x))
 
         # print("Liberties: " + str(len(self.liberties)) + " in: " + str(x) + "," + str(y))
         # print("Block: " + str(len(self.block)) + " in: " + str(x) + "," + str(y))
-        return self.liberties    
+        return liberties, block
 
     #remove captured stones
-    def clear_block(self):
+    def clear_block(self, block: list, state: list) -> list:
+        '''
+        # Description:
+        Clears the block of stones captured by the opponent on the board.
+
+        # Returns:
+        The board with the captured stones removed.
+        '''
 
         #clears the elements in the block of elements which is captured
-        for i in range(len(self.block)): 
-            y = self.block[i][0]
-            x = self.block[i][1]
-            self.board[y][x] = self.EMPTY
-
-    #clear groups
-    def clear_groups(self):
-
-        #clear block and liberties lisits
-        self.block = []
-        self.liberties = []
+        for i in range(len(block)): 
+            y, x = block[i]
+            state[y][x] = self.EMPTY
+        
+        return state
 
     #restore board after counting stones and liberties
-    def restore_board(self):
-        #clear groups
-        self.clear_groups()
+    def restore_board(self, state: list) -> list:
+        '''
+        # Description:
+        Restores the board to its original state after counting liberties and stones.
+        This is done by unmarking the stones following bitwise operations with the global class variables.
+        
+        # Returns:
+        The board with the stones unmarked.
+        '''
 
         #unmark stones
-        for y in range(len(self.board)):
-            for x in range(len(self.board)):
+        for y in range(len(state)):
+            for x in range(len(state)):
                 #restore piece
-                val = self.board[y][x]
+                val = state[y][x]
                 new_val = val & 3
-                self.board[y][x] = new_val
+                state[y][x] = new_val
+        return state
 
-    #clear board
-    def clear_board(self):
-        #clear groups
-        self.clear_groups()
+    def draw_board(self, state) -> None:
+            '''
+            # Description:
+            Draws the board in the console.
 
-        #clears the board
-        for y in range(len(self.board)):
-            for x in range(len(self.board)):
-                self.board[y][x] = 0
-
-    def draw_board(self) -> None:
+            # Returns:
+            None
+            '''
 
         # Print column coordinates
             print("   ", end="")
-            for j in range(len(self.board[0])):
+            for j in range(len(state[0])):
                 print(f"{j:2}", end=" ")
             print("\n  +", end="")
-            for _ in range(len(self.board[0])):
+            for _ in range(len(state[0])):
                 print("---", end="")
             print()
 
             # Print rows with row coordinates
-            for i in range(len(self.board)):
+            for i in range(len(state)):
                 print(f"{i:2}|", end=" ")
-                for j in range(len(self.board[0])):
-                    print(f"{str(self.board[i][j]):2}", end=" ")
+                for j in range(len(state[0])):
+                    print(f"{str(state[i][j]):2}", end=" ")
                 print()
 
-    def captures(self,color):
+    def captures(self, state: list,player: int) -> tuple[bool, list]:
+        '''
+        # Description:
+        Checks if a move causes a capture of stones of the player passed as an argument.
+        If a move causes a capture, the stones are removed from the board.
+
+        # Returns:
+        A tuple containing a boolean indicating if a capture has been made and the board with the captured stones removed.
+        '''
         check = False
 
         #loop over the board squares
-        for y in range(len(self.board)):
-            for x in range(len(self.board)):
+        for y in range(len(state)):
+            for x in range(len(state)):
                 
                 #init piece
-                piece = self.board[y][x]
+                piece = state[y][x]
 
                 #if stone belongs to given colour
-                if piece & color:
+                if piece & player:
                     
                     #count liberties
-                    self.count(x,y,color)
+                    liberties = []
+                    block = []
+                    liberties, block = self.count(x, y, state, player, liberties, block)
 
                     #if no liberties remove the stones
-                    if len(self.liberties) == 0: 
+                    if len(liberties) == 0: 
                         
-                        prisioners = len(self.block)
+                        prisioners = len(block)
                         #clear block
-                        self.clear_block()
+                        state = self.clear_block(block, state)
 
-                        if color == self.BLACK:
+                        if player == self.BLACK:
                             self.black_captures += prisioners
                         else:
                             self.white_captures += prisioners
@@ -161,94 +215,161 @@ class Go:
                             continue
 
                     #restore the board
-                    self.restore_board()
-        # print("Seki Count: " + str(self.seki_count))
-        #print("Captures: " + str(check))
-        return check
+                    state = self.restore_board(state)
+        print("Seki Count: " + str(self.seki_count))
+        print("Captures: " + str(check))
+        return check, state
 
-    def change_player(self):
-        
-            #switching colours for the next move
-            if self.color == self.BLACK:
-                self.color = self.WHITE
-            else:
-                self.color = self.BLACK
+    def change_player(self, player: int) -> int:
+        '''
+        # Description:
+        Changes the player.
 
-    def get_player(self):
-        return self.color
+        # Returns:
+        The other player.
+        '''
+        #switching colours for the next move
+        if player == self.BLACK:
+            player = self.WHITE
+        else:
+            player = self.BLACK
+            
+        return player
 
-    def set_stone(self,y,x):
-        #making move on the board
-        self.board[y][x] = self.color
+    def set_stone(self, y: int, x: int, state: list, player: int) -> list:
+        '''
+        # Description:
+        Sets a stone on the board.
 
-    def is_valid_move(self, action):
+        # Returns:
+        The board with the stone set.
+        '''
 
-        pre_board = numpy.copy(self.board)
+        state[y][x] = player
+        return state
+
+    def is_valid_move(self, state: list, action: tuple, player: int) -> bool:
+        '''
+        # Description:
+        Checks if a move is valid.
+        If a move repeats a previous state or commits suicide (gets captured without capturing back), it is not valid.
+
+        # Returns:
+        A boolean confirming the validity of the move.
+        '''
+
+        statecopy = numpy.copy(state)
         a, b = action
-        self.set_stone(a,b)
+        statecopy = self.set_stone(a,b, statecopy, player)
 
-        self.captures(3 - self.get_player())
-        str_board = self.board_to_str()
+        statecopy = self.captures(statecopy, 3 - player)[1]
+        str_board = self.board_to_str(statecopy)
 
         if str_board in self.history:
             print("Invalid Move: Repeated State")
-            self.board = numpy.copy(pre_board)
             return False
 
         if self.seki_count == 1:
             if str_board in self.history:
                 print("Invalid Move: Repeated State")
-                self.board = numpy.copy(pre_board)
                 return False
         else:
-            if self.captures(3-self.get_player()) == False and self.captures(self.get_player()) == True:
+            if self.captures(statecopy, 3-player)[0] == False and self.captures(statecopy, player)[0] == True:
                 print("Invalid Move: Suicide")
-                self.board = numpy.copy(pre_board)
                 return False
             
-        self.board = numpy.copy(pre_board)  
         return True
     
-    def board_to_str(self):
-        return ''.join([''.join(map(str, row)) for row in self.board])
+    def board_to_str(self, state: list) -> str:
+        '''
+        # Description:
+        Converts the board to a string representation.
 
-    def save_state(self):
-        str_board = self.board_to_str()
+        # Returns
+        String representation of the board.
+        '''
+        return ''.join([''.join(map(str, row)) for row in state])
+
+    def save_state(self, state: list) -> None:
+        '''
+        # Description:
+        Saves the board into a list of string representations of boards.
+        
+        # Returns:
+        None
+        '''
+        str_board = self.board_to_str(state)
         self.history.append(str_board)  # Save string to history list
 
-    def get_next_state(self, action):
+    def get_next_state(self, state: list, action: tuple, player: int) -> list:
+        '''
+        # Description
+        Plays the move, verifies and undergoes captures and saves the state to the history.
+        
+        # Returns:
+        New state with everything updated.
+        '''
         a, b = action
         # checking if the move is part of is the secondary move to a ko fight
-        self.set_stone(a, b)
-        self.captures(3-self.get_player())
-        self.save_state()
-        return self.board
+        state = self.set_stone(a, b, state, player)
+        state = self.captures(state, 3 - player)[1]
+        self.save_state(state)
+        return state
 
-    def check_board_full(self):
+    def check_board_full(self, state: list) -> bool:
+        '''
+        # Description:
+        Checks if the board is full.
+
+        # Returns:
+        True if the board is full.
+        '''
 
         empty_count = 0
-        for row in self.board:
+        for row in state:
             for stone in row:
                 if stone == 0:
                     empty_count += 1
-                if empty_count == 2:
-                    return True
+                if empty_count >= 3:
+                    return False
                 
-        return False
+        return True
 
-    def check_win_and_over(self):
+    def check_win_and_over(self, state: list) -> tuple[int, bool]:
+        '''
+        # Description:
+        Checks if the game is over and if there is a winner.
 
-        if self.check_board_full():
-            if self.black_captures >= self.white_captures + self.komi:
+        # Returns:
+        A tuple containing the winner and a boolean indicating if the game is over.
+        '''
+        if self.check_skip_end(action):
+            points = self.check_score(state)
+            if points > 0:
                 return 1, True
-            else: 
-                return 1, False
+            else:
+                return -1, True
+
+        if self.check_board_full(state):
+            points = self.check_score(state)
+            if points > 0:
+                return 1, True
+            else:
+                return -1, True
         
-        return 1, False
+        return 0, False
     
-    def check_score(self):
-        black_pieces, white_pieces = 0
-        for row in self.board:
+    def check_score(self, state: list) -> float:
+        '''
+        # Description:
+        Checks the score of the game.
+
+        # Returns:
+        The score of the game, positive if black is winning, negative if white is winning.
+        '''
+        black_pieces = 0
+        white_pieces = 0
+        for row in state:
             for stone in row:
                 if stone == 1:
                     black_pieces += 1
@@ -260,16 +381,31 @@ class Go:
 
         return black_points - white_points
     
-    def get_value_and_terminated(self):
-        return self.check_score(), self.check_board_full()
+    def get_value_and_terminated(self,state: list) -> tuple[float, bool]:
+        '''
+        # Description:
+        Gets the score of the game and if the game is over.
+
+        # Returns:
+        A tuple containing the score of the game and a boolean indicating if the game is over.
+        '''
+        return self.check_score(state), self.check_board_full(state)
     
-    def get_valid_moves(self):
+    def get_valid_moves(self,state: list, player: int) -> set[tuple[int, int]]:
+        '''
+        # Description:
+        Gets the valid moves for a given state.
+
+        # Returns:
+        A set containing the valid moves.
+        '''
         possible_moves = set()
 
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                if self.board[i][j] == 0:
-                    possible_moves.add((i, j))
+        for i in range(len(state)):
+            for j in range(len(state)):
+                action = (i, j)
+                if self.is_valid_move(state, action, player):
+                    possible_moves.add(action)
         
         return possible_moves
 
@@ -277,17 +413,22 @@ class Go:
 args = [9, 5.5]
 
 go = Go(args)
-
-go.draw_board()
+state = go.get_initial_state()
+go.draw_board(state)
+player = 1
 
 while True:
-    print("Player: " + str(go.get_player()))
+    print("Player: " + str(player))
     print("Input: ")
     a, b = tuple(int(x.strip()) for x in input().split(' '))
     action = (a, b)
-    if go.is_valid_move(action):
-        go.board = go.get_next_state(action)
-        go.change_player()
+    if go.is_valid_move(state, action, player):
+        state = go.get_next_state(state, action, player)
+        winner, win = go.check_win_and_over(state)
+        if win:
+            print("Winner: " + str(winner))
+            break
+        player = go.change_player(player)
         
 
-    go.draw_board()
+    go.draw_board(state)
