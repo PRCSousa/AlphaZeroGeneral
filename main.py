71b6da75b96e65f8ff1ebe4f94e3137267b15096
Pedro "Totty" Sousa <83694082+TottyAmadeus@users.noreply.github.com>
2023-12-24 13:10:05 +0000

    augmented date x8

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ if __name__ == '__main__':
             'game': 'Go',
             'num_iterations': 20,             # number of highest level iterations
             'num_selfPlay_iterations': 20,   # number of self-play games to play within each iteration
-            'max_moves': 20,                 # maximum number of moves before a game is terminated
-            'num_mcts_searches': 50,         # number of mcts simulations when selecting a move within self-play
-            'num_epochs': 20,                  # number of epochs for training on self-play data for each iteration
+            'max_moves': 30,                 # maximum number of moves before a game is terminated
+            'num_mcts_searches': 40,         # number of mcts simulations when selecting a move within self-play
+            'num_epochs': 800,                  # number of epochs for training on self-play data for each iteration
             'batch_size': 2,                # batch size for training, HAS to be smaller than num_selfPlay_iterations
             'temperature': 1.25,              # temperature for the softmax selection of moves
             'C': 2,                           # the value of the constant policy
