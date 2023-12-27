4ef6759052f1c336c13e56c27d9e57a4165239a0
Barbara Santos <pro@barbarasan.com>
2023-12-27 13:52:51 +0000

    attaxx alegadamente funciona

--- AlphaZero/alphaZero.py
+++ AlphaZero/alphaZero.py
@@ -177,6 +177,7 @@ class MCTS:
         policy *= valid_moves
         policy /= np.sum(policy)
         root.expand(policy)
+        #print(valid_moves)
         
         for search in range(self.args['num_mcts_searches']):
             node = root
@@ -192,10 +193,7 @@ class MCTS:
                     is_terminal = True # if the action is pass when the previous action was also pass, end the game
 
             if not is_terminal:
-                
-                
-                policy, value = self.model(torch.tensor(self.game.get_encoded_state(node.state), device=self.model.device).unsqueeze(0)
-                )
+                policy, value = self.model(torch.tensor(self.game.get_encoded_state(node.state), device=self.model.device).unsqueeze(0))
                 policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
                 #print("POLICY:", policy)
                 valid_moves = self.game.get_valid_moves(node.state, player)
@@ -204,10 +202,12 @@ class MCTS:
                 #print("POLICY AFTER *valid_moves:", policy)
 
                 if np.sum(policy) == 0 and self.args["game"] == "Attaxx":
-                    #print("VALID MOVES:", valid_moves)
-                    #self.game.print_board(node.state)
                     node.state = self.game.change_perspective(node.state, player=-1)
                     continue
+                    '''policy, value = self.model(torch.tensor(self.game.get_encoded_state(node.state), device=self.model.device).unsqueeze(0))
+                    policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+                    valid_moves = self.game.get_valid_moves(node.state, player)
+                    policy *= valid_moves'''
 
                 policy /= np.sum(policy)
                 
---
+++ notas_attax.md
@@ -0,0 +1,17 @@
+# Testes de Modelos
+
+## Modelo 1 | Attaxx5_8_100_100_4_128
+
+Formato:
+- 'board_size': 5x5
+- 'num_iterations': 8
+- 'num_selfPlay_iterations': 100
+- 'num_mcts_searches': 100
+- 'num_epochs': 4
+- 'batch_size': 128,
+
+Joga bastante mal, ganhei-lhe em 4 jogadas. Vou diminuir o numero de epocas para metade para ficar mais perto do AlphaZero original e aumentar as searches e iterações.
+
+## Modelo 1 | Attaxx5_20_200_1000_2_128
+
+Em treinamento.
